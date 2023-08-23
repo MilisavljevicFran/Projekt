331872a4104ba8d2c6ff573106712eb2389d25b7
fran <fmilisavl@tvz.hr>
2023-08-23 23:24:04 +0200

    nezz, ima jedan error ali radi

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from "@angular/core";
+import { IProperty } from "../iproperty.interface";
 
 @Component({
     selector: 'app-property-card',
-    //template: `<h1>I am a card</h1>`,
     templateUrl: 'property-card.component.html',
-    //styles: ['h1{font-weight: normal}']
     styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
 
-    @Input() property : any;
+    @Input() property : IProperty;
 
 }
+// piše da je error ali uspješno kompajlira
