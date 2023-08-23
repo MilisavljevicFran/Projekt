6d1848e8e7ae95fce40cec53a6c00b23bffaa6e3
fran <fmilisavl@tvz.hr>
2023-08-23 13:41:44 +0200

    property list funkcionalnost

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-property-card',
@@ -8,10 +8,7 @@ import { Component } from "@angular/core";
     styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
-    Property: any = {
-        "Id":1,
-        "Name":"Habibi",
-        "Type":"House",
-        "Price":12000
-    }
+
+    @Input() property : any;
+
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+    Properties: Array<any> = [
+    	{
+    	    "Id":1,
+    		"Name":"Birla house",
+    		"Type":"House",
+    		"Price":12000
+  		},
+		{
+			"Id":2,
+			"Name":"Erose villa",
+			"Type":"House",
+			"Price":13000
+		},
+		{
+			"Id":3,
+			"Name":"Bruv",
+			"Type":"den",
+			"Price":52000
+		},
+		{
+			"Id":4,
+			"Name":"Ugly castle",
+			"Type":"castle",
+			"Price":2000
+		}
+]
+
 }
